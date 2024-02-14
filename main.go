954eee10984846b1c3e233580b823a6a37773278
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-02-14 14:15:24 +0100

    More work

--- main.go
+++ main.go
@@ -6,28 +6,27 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 
 	"github.com/kr/pretty"
 	"github.com/pelletier/go-toml/v2"
 )
 
 type Track struct {
-	Filename string `toml:"filename"`
-	Name     string `toml:"name"`
+	Filename string   `toml:"filename"`
+	CoverArt string   `toml:"coverart"`
+	Name     string   `toml:"name"`
+	Date     string   `toml:"date"`
+	Tags     []string `toml:"tags"`
 }
 
 type Config struct {
-	Artist string `toml:"artist"` 
+	Artist string  `toml:"artist"`
 	Tracks []Track `toml:"tracks"`
 }
 
 func main() {
-	dir := "./result"
-	err := os.MkdirAll(dir, 0755)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	dstDir := "./result"
 	srcDir := "/Volumes/media/musicraw/Divide Music"
 
 	data, err := os.ReadFile(path.Join(srcDir, "tracks.toml"))
@@ -43,14 +42,43 @@ func main() {
 
 	pretty.Println(config)
 
+	// TODO(patrik): Check albumName for forward slashes and other illegal
+	// filesystem characters
 	for _, track := range config.Tracks {
-		cmd := exec.Command("ffmpeg", 
-			"-i", path.Join(srcDir, track.Filename), 
-			"-metadata", fmt.Sprintf(`title=%s`, track.Name), 
-			"-metadata", fmt.Sprintf(`artist=%s`, config.Artist), 
-			"-metadata", fmt.Sprintf(`album_artist=%s`, config.Artist), 
-			"-metadata", fmt.Sprintf(`album=%s`, track.Name + " (Single)"), 
-			path.Join(dir, track.Name+".flac"))
+		fmt.Printf("track.Name: %v\n", track.Name)
+
+		albumName := track.Name + " (Single)"
+
+		dir := path.Join(dstDir, albumName)
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		src := path.Join(srcDir, track.CoverArt)
+		srcFile, err := os.Open(src)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_ = srcFile
+
+		args := []string{}
+
+		args = append(args, "-i", path.Join(srcDir, track.Filename))
+
+		args = append(args, "-metadata", fmt.Sprintf(`title=%s`, track.Name))
+		args = append(args, "-metadata", fmt.Sprintf(`artist=%s`, config.Artist))
+		args = append(args, "-metadata", fmt.Sprintf(`album_artist=%s`, config.Artist))
+		args = append(args, "-metadata", fmt.Sprintf(`album=%s`, track.Name+" (Single)"))
+		args = append(args, "-metadata", fmt.Sprintf(`tags=%s`, strings.Join(track.Tags, ",")))
+		if track.Date != "" {
+			args = append(args, "-metadata", fmt.Sprintf(`date=%s`, track.Date))
+		}
+
+		args = append(args, path.Join(dir, track.Name+".flac"))
+
+		cmd := exec.Command("ffmpeg", args...)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 
