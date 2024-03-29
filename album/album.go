58d8b40da447246e3cc8aa05a3c0fd7c866ee6b8
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-03-29 16:54:57 +0100

    Testing error message

--- album/album.go
+++ album/album.go
@@ -44,18 +44,18 @@ func Execute(mode, src, dst string) error {
 
 	data, err := os.ReadFile(conf)
 	if err != nil {
-		return err
+		return fmt.Errorf("%s: %w", err)
 	}
 
 	var config AlbumConfig
 	err = toml.Unmarshal(data, &config)
 	if err != nil {
-		return err
+		return fmt.Errorf("%s: %w", err)
 	}
 
 	err = ExecuteConfig(config, mode, src, dst)
 	if err != nil {
-		return err
+		return fmt.Errorf("%s: %w", err)
 	}
 
 	return nil
